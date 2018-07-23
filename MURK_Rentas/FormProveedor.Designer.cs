3f6ceab92d5689a93e512a5f8982b26c2725cc29
Krevin17 <angel.96.dark@gmail.com>
2018-07-23 18:39:52 -0500

    Cambio a aceptar

--- MURK_Rentas/FormProveedor.Designer.cs
+++ MURK_Rentas/FormProveedor.Designer.cs
@@ -386,7 +386,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 30);
             this.button2.TabIndex = 33;
-            this.button2.Text = "Guardar";
+            this.button2.Text = "Listo";
             this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
